33f15951fd5ddb5ae19e180a915a7c7c7f922730
Annie Huang <annie.hn@gmail.com>
2019-01-03 20:03:52 +1100

    Added task list component

--- src/app/tasks/task.module.ts
+++ src/app/tasks/task.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { TaskComponent } from './task.component';
 import {CommonModule} from '@angular/common';
+import {TaskListComponent} from './task-list.component';
 
 @NgModule({
   imports: [CommonModule],
-  exports: [TaskComponent],
-  declarations: [TaskComponent],
+  exports: [TaskComponent, TaskListComponent],
+  declarations: [TaskComponent, TaskListComponent],
   providers: [],
 })
 export class TaskModule {}
